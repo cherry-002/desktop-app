99a6eda3ab7f2a6b8c6dee0c59371fca728bd68d
cherry-002 <alirezaesmaili6040@gmail.com>
2024-11-12 23:59:13 +0330

    adding alphabets

---
+++ public/upper-case/index.js
@@ -0,0 +1,32 @@
+
+import A from './A.svg';
+import B from './B.svg';
+import C from './C.svg';
+import D from './D.svg';
+import E from './E.svg';
+import F from './F.svg';
+import G from './G.svg';
+import H from './H.svg';
+import I from './I.svg';
+import J from './J.svg';
+import K from './K.svg';
+import L from './L.svg';
+import M from './M.svg';
+import N from './N.svg';
+import O from './O.svg';
+import P from './P.svg';
+import Q from './Q.svg';
+import R from './R.svg';
+import S from './S.svg';
+import T from './T.svg';
+import U from './U.svg';
+import V from './V.svg';
+import W from './W.svg';
+import X from './X.svg';
+import Y from './Y.svg';
+import Z from './Z.svg';
+
+
+export const upperCaseArray = [
+  A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+];
